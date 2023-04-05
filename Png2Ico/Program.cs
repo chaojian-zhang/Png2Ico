73c03954dd021e7012e5e270f29b62bb2cc51362
Charles Zhang <charles_zhang@otpp.com>
2023-04-05 13:22:09 -0400

    Add project files.

---
+++ Png2Ico/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Runtime.Versioning;
+
+namespace Png2Ico
+{
+    [SupportedOSPlatform("windows")]
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: PngToIcoConverter <sourceFilePath> <destinationFilePath>");
+                return;
+            }
+
+            string sourceFilePath = args[0];
+            string destinationFilePath = args[1];
+
+            if (!File.Exists(sourceFilePath))
+            {
+                Console.WriteLine($"Error: Source file '{sourceFilePath}' does not exist.");
+                return;
+            }
+
+            try
+            {
+                ConvertPngToIco(sourceFilePath, destinationFilePath);
+                Console.WriteLine($"'{sourceFilePath}' has been successfully converted to '{destinationFilePath}'.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private static void ConvertPngToIco(string sourceFilePath, string destinationFilePath)
+        {
+            using (FileStream stream = File.OpenWrite(destinationFilePath))
+            {
+                Bitmap bitmap = FixSize((Bitmap)Image.FromFile(sourceFilePath));
+                Icon.FromHandle(bitmap.GetHicon()).Save(stream);
+            }
+        }
+
+        private static Bitmap FixSize(Bitmap old)
+        {
+            if (old.Size.Width > 256 || old.Size.Height > 256)
+                return new Bitmap(old, new Size(256, 256));
+            return old;
+        }
+    }
+}
